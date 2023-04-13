3196c3fcae397f6aa83ead5d2b2005caa89b9e6a
ymaktepi <ymaktepi@users.noreply.github.com>
2023-04-13 21:00:54 +0000

    deploy: 5accccd692df367229f2c07a58042af8ca062f58

--- ts/app/main-books.js
+++ ts/app/main-books.js
@@ -1,20 +1,18 @@
-(()=>{var a=class{url;constructor(e){this.url=e}getSheetData(){return fetch(this.url).then(e=>e.json()).then(this.parseData)}parseData(e){let t=e.values;return t.pop(),t.map(r=>({isbn:r[0],title:r[1],author:r[2],year:r[3]}))}async getAndParseBooks(){return await this.getSheetData()}};var l="https://script.google.com/macros/s/AKfycbzBWJ74xhT8Dk7adHsYwCIRjZbWCYu8uYKNWYPFjE6GyS_ejJP5HTj-Uz-1AyAidABZ/exec";var o=class{constructor(){}fillElement(e,t){e.innerHTML=this.generateTable(t)}generateTableRow(e){return`<tr>
-      <th scope="row">${e.isbn}</th>
-      <td>${e.author}</td>
-      <td>${e.title}</td>
-      <td>${e.year}</td>
-    </tr>`}generateTableElement(e){return`
-        <table class="table table-striped table-sm table-hover">
-  <thead>
-    <tr>
-      <th scope="col">ISBN</th>
-      <th scope="col">Auteur</th>
-      <th scope="col">Titre</th>
-      <th scope="col">Ann\xE9e</th>
-    </tr>
-  </thead>
-  <tbody>
-  ${e}
-  </tbody>
-</table>
-        `}generateTable(e){let t=e.map(this.generateTableRow);return this.generateTableElement(t.join(""))}};async function n(){let e=await new a(l).getAndParseBooks();new o().fillElement(document.getElementById("books-div"),e)}n();})();
+(()=>{var s=class{url;constructor(t){this.url=t}getSheetData(){return fetch(this.url).then(t=>t.json()).then(this.parseData)}parseData(t){let r=t.values;return r.shift(),r.map(e=>({isbn:e[0],title:e[1],author:e[2],publishingDate:e[3],theme:e[4],trigger:e[5],status:e[6],lendingDate:e[7].split("T")[0]}))}async getAndParseBooks(){return await this.getSheetData()}};var i="https://script.google.com/macros/s/AKfycbzBWJ74xhT8Dk7adHsYwCIRjZbWCYu8uYKNWYPFjE6GyS_ejJP5HTj-Uz-1AyAidABZ/exec";var l=class{constructor(){}fillElement(t,r){t.innerHTML=this.generateTableElement();let e=new DataTable("#book-table",{data:r.map(a=>[a.isbn,a.title,a.author,a.publishingDate,a.theme,a.trigger,a.status,a.lendingDate]),language:{url:"https://cdn.datatables.net/plug-ins/1.13.4/i18n/fr-FR.json"}})}generateTableElement(){return`
+            <table id="book-table" class="table table-striped table-sm table-hover">
+              <thead>
+                <tr>
+                  <th scope="col">Cote</th>
+                  <th scope="col">Auteur&middot;trice</th>
+                  <th scope="col">Titre</th>
+                  <th scope="col">Date de publication</th>
+                  <th scope="col">Th\xE8me</th>
+                  <th scope="col">Trigger</th>
+                  <th scope="col">Status</th>
+                  <th scope="col">Date d'emprunt</th>
+                </tr>
+              </thead>
+              <tbody>
+              </tbody>
+            </table>
+        `}};async function n(){let t=await new s(i).getAndParseBooks();new l().fillElement(document.getElementById("books-div"),t)}n();})();
