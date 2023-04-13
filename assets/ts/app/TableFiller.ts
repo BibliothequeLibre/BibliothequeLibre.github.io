49f2392f4b23bb9cdb1df7a2ab8c7bea2fe3f7c5
Nicolas Huguenin <nicolas@unit8.co>
2023-04-13 22:48:17 +0200

    feat: added data tables

--- assets/ts/app/TableFiller.ts
+++ assets/ts/app/TableFiller.ts
@@ -6,7 +6,11 @@ export class TableFiller {
 
     public fillElement(elementToFill: HTMLElement, books: Array<Book>) {
         elementToFill.innerHTML = this.generateTable(books);
-
+        let table = new DataTable('#book-table', {
+            language: {
+                url: 'https://cdn.datatables.net/plug-ins/1.13.4/i18n/fr-FR.json',
+            },
+        });
     }
 
     private generateTableRow(book: Book): string {
@@ -14,19 +18,27 @@ export class TableFiller {
       <th scope="row">${book.isbn}</th>
       <td>${book.author}</td>
       <td>${book.title}</td>
-      <td>${book.year}</td>
+      <td>${book.publishingDate}</td>
+      <td>${book.theme}</td>
+      <td>${book.trigger}</td>
+      <td>${book.status}</td>
+      <td>${book.lendingDate}</td>
     </tr>`
     }
 
     private generateTableElement(tableContent: string) {
         return `
-        <table class="table table-striped table-sm table-hover">
+        <table id="book-table" class="table table-striped table-sm table-hover">
   <thead>
     <tr>
-      <th scope="col">ISBN</th>
-      <th scope="col">Auteur</th>
+      <th scope="col">Cote</th>
+      <th scope="col">Auteur&middot;trice</th>
       <th scope="col">Titre</th>
-      <th scope="col">Année</th>
+      <th scope="col">Date de publication</th>
+      <th scope="col">Thème</th>
+      <th scope="col">Trigger</th>
+      <th scope="col">Status</th>
+      <th scope="col">Date d'emprunt</th>
     </tr>
   </thead>
   <tbody>
