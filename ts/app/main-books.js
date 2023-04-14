0d1fbdb10d5751e945b89da1b8e889ec20082b95
ymaktepi <ymaktepi@users.noreply.github.com>
2023-04-14 16:40:16 +0000

    deploy: 424742815a6d9d01ed332a05393f3dec4213c4d3

--- ts/app/main-books.js
+++ ts/app/main-books.js
@@ -1,9 +1,9 @@
-(()=>{var s=class{url;constructor(e){this.url=e}getSheetData(){return fetch(this.url).then(e=>e.json()).catch(e=>(console.log("Could not get response from url:",e),{values:["shifted"]})).then(this.parseData)}parseData(e){let a=e.values;return a.shift(),a.map(t=>({isbn:t[0],title:t[1],author:t[2],publishingDate:t[3],theme:t[4],trigger:t[5],status:t[6],lendingDate:t[7].split("T")[0]}))}async getAndParseBooks(){return await this.getSheetData()}};var n="https://script.google.com/macros/s/AKfycbwH8daIpK6TD7-kqrF-0rgsgU9LzGceIsebvGWnbesVGDx2rwN5hH7CZNP1cyUXpVu0tg/exec";var o=class{constructor(){}fillElement(e,a){if(a.length==0){e.innerHTML=`<div class="alert alert-warning" role="alert">
+(()=>{var a=class{url;constructor(t){this.url=t}getSheetData(){return fetch(this.url).then(t=>t.json()).catch(t=>(console.log("Could not get response from url:",t),{values:["shifted"]})).then(this.parseData)}parseData(t){let r=t.values;return r.shift(),r.map(e=>({isbn:e[0],title:e[1],author:e[2],publishingDate:e[3],theme:e[4],trigger:e[5],status:e[6],lendingDate:e[7].split("T")[0]}))}async getAndParseBooks(){return await this.getSheetData()}};var o="https://script.google.com/macros/s/AKfycbwH8daIpK6TD7-kqrF-0rgsgU9LzGceIsebvGWnbesVGDx2rwN5hH7CZNP1cyUXpVu0tg/exec";var s=class{constructor(){}fillElement(t,r){if(r.length==0){t.innerHTML=this.generateWarningMessageIfEmptyBooks();return}t.innerHTML=this.generateTableElement(),new DataTable("#book-table",{data:r.map(e=>[e.isbn,e.title,e.author,e.publishingDate,e.theme,e.trigger,e.status,e.lendingDate]),language:{url:"https://cdn.datatables.net/plug-ins/1.13.4/i18n/fr-FR.json"}})}generateWarningMessageIfEmptyBooks(){return`<div class="alert alert-warning" role="alert">
                 Aucun livre n'a \xE9t\xE9 trouv\xE9. Activez Javascript et assurez vous que vous n'avez pas
                 d'extensions de navigateurs qui bloquent l'acc\xE8s a Google Scripts (par exemple avec Firefox: 'Privacy Badger' ou
                 'DuckDuckGo Privacy Extensions'). Si en changeant vos param\xE8tres l'erreur persiste, merci de contacter
                 la personne responsable.
-                </div>`;return}e.innerHTML=this.generateTableElement();let t=new DataTable("#book-table",{data:a.map(r=>[r.isbn,r.title,r.author,r.publishingDate,r.theme,r.trigger,r.status,r.lendingDate]),language:{url:"https://cdn.datatables.net/plug-ins/1.13.4/i18n/fr-FR.json"}})}generateTableElement(){return`
+                </div>`}generateTableElement(){return`
             <table id="book-table" class="table table-striped table-sm table-hover">
               <thead>
                 <tr>
@@ -20,4 +20,4 @@
               <tbody>
               </tbody>
             </table>
-        `}};async function i(){let e=await new s(n).getAndParseBooks();new o().fillElement(document.getElementById("books-div"),e)}i();})();
+        `}};async function i(){let t=await new a(o).getAndParseBooks();new s().fillElement(document.getElementById("books-div"),t)}i();})();
