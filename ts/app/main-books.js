819ffd9f5033f2d1c38b74d465b90475dd01069d
ymaktepi <ymaktepi@users.noreply.github.com>
2023-04-14 16:30:17 +0000

    deploy: 34dbc260ceb5c7c4e709ae19197cf6ee9abccd96

--- ts/app/main-books.js
+++ ts/app/main-books.js
@@ -1,9 +1,9 @@
-(()=>{var s=class{url;constructor(e){this.url=e}getSheetData(){return fetch(this.url).then(e=>e.json()).catch(e=>(console.log("Could not get response from url:",e),{values:["shifted"]})).then(this.parseData)}parseData(e){let r=e.values;return r.shift(),r.map(t=>({isbn:t[0],title:t[1],author:t[2],publishingDate:t[3],theme:t[4],trigger:t[5],status:t[6],lendingDate:t[7].split("T")[0]}))}async getAndParseBooks(){return await this.getSheetData()}};var l="https://script.google.com/macros/s/AKfycbzBWJ74xhT8Dk7adHsYwCIRjZbWCYu8uYKNWYPFjE6GyS_ejJP5HTj-Uz-1AyAidABZ/exec";var o=class{constructor(){}fillElement(e,r){if(r.length==0){e.innerHTML=`<div class="alert alert-warning" role="alert">
+(()=>{var s=class{url;constructor(e){this.url=e}getSheetData(){return fetch(this.url).then(e=>e.json()).catch(e=>(console.log("Could not get response from url:",e),{values:["shifted"]})).then(this.parseData)}parseData(e){let a=e.values;return a.shift(),a.map(t=>({isbn:t[0],title:t[1],author:t[2],publishingDate:t[3],theme:t[4],trigger:t[5],status:t[6],lendingDate:t[7].split("T")[0]}))}async getAndParseBooks(){return await this.getSheetData()}};var n="https://script.google.com/macros/s/AKfycbwH8daIpK6TD7-kqrF-0rgsgU9LzGceIsebvGWnbesVGDx2rwN5hH7CZNP1cyUXpVu0tg/exec";var o=class{constructor(){}fillElement(e,a){if(a.length==0){e.innerHTML=`<div class="alert alert-warning" role="alert">
                 Aucun livre n'a \xE9t\xE9 trouv\xE9. Activez Javascript et assurez vous que vous n'avez pas
                 d'extensions de navigateurs qui bloquent l'acc\xE8s a Google Scripts (par exemple avec Firefox: 'Privacy Badger' ou
                 'DuckDuckGo Privacy Extensions'). Si en changeant vos param\xE8tres l'erreur persiste, merci de contacter
                 la personne responsable.
-                </div>`;return}e.innerHTML=this.generateTableElement();let t=new DataTable("#book-table",{data:r.map(a=>[a.isbn,a.title,a.author,a.publishingDate,a.theme,a.trigger,a.status,a.lendingDate]),language:{url:"https://cdn.datatables.net/plug-ins/1.13.4/i18n/fr-FR.json"}})}generateTableElement(){return`
+                </div>`;return}e.innerHTML=this.generateTableElement();let t=new DataTable("#book-table",{data:a.map(r=>[r.isbn,r.title,r.author,r.publishingDate,r.theme,r.trigger,r.status,r.lendingDate]),language:{url:"https://cdn.datatables.net/plug-ins/1.13.4/i18n/fr-FR.json"}})}generateTableElement(){return`
             <table id="book-table" class="table table-striped table-sm table-hover">
               <thead>
                 <tr>
@@ -20,4 +20,4 @@
               <tbody>
               </tbody>
             </table>
-        `}};async function n(){let e=await new s(l).getAndParseBooks();new o().fillElement(document.getElementById("books-div"),e)}n();})();
+        `}};async function i(){let e=await new s(n).getAndParseBooks();new o().fillElement(document.getElementById("books-div"),e)}i();})();
