23dadd5d4297d9fd236a330ebdce2493ccb992be
ymaktepi <ymaktepi@users.noreply.github.com>
2023-04-14 10:59:06 +0000

    deploy: 2a5b1dfdd1a0e2dc5c83d472b7efe90d1ed8acc3

--- ts/app/main-books.js
+++ ts/app/main-books.js
@@ -1,4 +1,9 @@
-(()=>{var s=class{url;constructor(t){this.url=t}getSheetData(){return fetch(this.url).then(t=>t.json()).then(this.parseData)}parseData(t){let r=t.values;return r.shift(),r.map(e=>({isbn:e[0],title:e[1],author:e[2],publishingDate:e[3],theme:e[4],trigger:e[5],status:e[6],lendingDate:e[7].split("T")[0]}))}async getAndParseBooks(){return await this.getSheetData()}};var i="https://script.google.com/macros/s/AKfycbzBWJ74xhT8Dk7adHsYwCIRjZbWCYu8uYKNWYPFjE6GyS_ejJP5HTj-Uz-1AyAidABZ/exec";var l=class{constructor(){}fillElement(t,r){t.innerHTML=this.generateTableElement();let e=new DataTable("#book-table",{data:r.map(a=>[a.isbn,a.title,a.author,a.publishingDate,a.theme,a.trigger,a.status,a.lendingDate]),language:{url:"https://cdn.datatables.net/plug-ins/1.13.4/i18n/fr-FR.json"}})}generateTableElement(){return`
+(()=>{var s=class{url;constructor(e){this.url=e}getSheetData(){return fetch(this.url).then(e=>e.json()).catch(e=>(console.log("Could not get response from url:",e),{values:["shifted"]})).then(this.parseData)}parseData(e){let r=e.values;return r.shift(),r.map(t=>({isbn:t[0],title:t[1],author:t[2],publishingDate:t[3],theme:t[4],trigger:t[5],status:t[6],lendingDate:t[7].split("T")[0]}))}async getAndParseBooks(){return await this.getSheetData()}};var l="https://script.google.com/macros/s/AKfycbzBWJ74xhT8Dk7adHsYwCIRjZbWCYu8uYKNWYPFjE6GyS_ejJP5HTj-Uz-1AyAidABZ/exec";var o=class{constructor(){}fillElement(e,r){if(r.length==0){e.innerHTML=`<div class="alert alert-warning" role="alert">
+                Aucun livre n'a \xE9t\xE9 trouv\xE9. Activez Javascript et assurez vous que vous n'avez pas
+                d'extensions de navigateurs qui bloquent l'acc\xE8s a Google Scripts (par exemple avec Firefox: 'Privacy Badger' ou
+                'DuckDuckGo Privacy Extensions'). Si en changeant vos param\xE8tres l'erreur persiste, merci de contacter
+                la personne responsable.
+                </div>`;return}e.innerHTML=this.generateTableElement();let t=new DataTable("#book-table",{data:r.map(a=>[a.isbn,a.title,a.author,a.publishingDate,a.theme,a.trigger,a.status,a.lendingDate]),language:{url:"https://cdn.datatables.net/plug-ins/1.13.4/i18n/fr-FR.json"}})}generateTableElement(){return`
             <table id="book-table" class="table table-striped table-sm table-hover">
               <thead>
                 <tr>
@@ -6,7 +11,7 @@
                   <th scope="col">Auteur&middot;trice</th>
                   <th scope="col">Titre</th>
                   <th scope="col">Date de publication</th>
-                  <th scope="col">Th\xE8me</th>
+                  <th scope="col">Th\xE9matique</th>
                   <th scope="col">Trigger</th>
                   <th scope="col">Status</th>
                   <th scope="col">Date d'emprunt</th>
@@ -15,4 +20,4 @@
               <tbody>
               </tbody>
             </table>
-        `}};async function n(){let t=await new s(i).getAndParseBooks();new l().fillElement(document.getElementById("books-div"),t)}n();})();
+        `}};async function n(){let e=await new s(l).getAndParseBooks();new o().fillElement(document.getElementById("books-div"),e)}n();})();
