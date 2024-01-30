4234d9632fead1b83568ae8730dbf25b5f37f135
ymaktepi <ymaktepi@users.noreply.github.com>
2024-01-30 18:42:39 +0000

    deploy: fe41c0176bbf1fe40c2a9f9440a47fd76a6d3aa2

--- ts/app/main-books.js
+++ ts/app/main-books.js
@@ -1,4 +1,4 @@
-(()=>{var r=class{url;constructor(e){this.url=e}getSheetData(){return fetch(this.url).then(e=>e.json()).catch(e=>(console.log("Could not get response from url:",e),{values:["shifted"]})).then(this.parseData)}parseData(e){let a=e.values;return a.shift(),a.map(t=>({isbn:t[0],title:t[1],author:t[2],publishingDate:t[3],theme:t[4],trigger:t[5],status:t[6],lendingDate:t[7].split("T")[0]}))}async getAndParseBooks(){return await this.getSheetData()}};var n="https://script.google.com/macros/s/AKfycbzSZC8h1J5219wrKfMRrR3KHrdYLFDOPVUZxUrm-TAvhaPRQfVnBe2gGbGOV0jyLjzOuQ/exec";var s=class{constructor(){}fillElement(e,a){if(a.length==0){e.innerHTML=this.generateWarningMessageIfEmptyBooks();return}e.innerHTML=this.generateTableElement(),new DataTable("#book-table",{data:a,columns:[{data:"isbn"},{data:"title",width:"30em"},{data:"author"},{data:"publishingDate"},{data:"theme"},{data:"trigger"},{data:"status",render:(t,i,c,p)=>`<span class="badge ${t=="disponible"?"bg-success":"bg-danger"}">${t}</span>`},{data:"lendingDate"}],language:{url:"https://cdn.datatables.net/plug-ins/1.13.4/i18n/fr-FR.json"}})}generateWarningMessageIfEmptyBooks(){return`<div class="alert alert-warning" role="alert">
+(()=>{var r=class{url;constructor(e){this.url=e}getSheetData(){return fetch(this.url).then(e=>e.json()).catch(e=>(console.log("Could not get response from url:",e),{values:["shifted"]})).then(this.parseData)}parseData(e){let a=e.values;return a.shift(),a.map(t=>({docType:t[0],category:t[1],isbn:""+t[2],title:t[3],author:t[4],year:t[5],editions:t[6]}))}async getAndParseBooks(){return await this.getSheetData()}};var n="https://script.google.com/macros/s/AKfycbzSZC8h1J5219wrKfMRrR3KHrdYLFDOPVUZxUrm-TAvhaPRQfVnBe2gGbGOV0jyLjzOuQ/exec";var o=class{constructor(){}fillElement(e,a){if(a.length==0){e.innerHTML=this.generateWarningMessageIfEmptyBooks();return}e.innerHTML=this.generateTableElement(),new DataTable("#book-table",{data:a,columns:[{data:"docType"},{data:"category"},{data:"isbn"},{data:"title",width:"30em"},{data:"author"},{data:"year"},{data:"editions"}],language:{url:"https://cdn.datatables.net/plug-ins/1.13.4/i18n/fr-FR.json"}})}generateWarningMessageIfEmptyBooks(){return`<div class="alert alert-warning" role="alert">
                 Aucun livre n'a \xE9t\xE9 trouv\xE9. Activez Javascript et assurez vous que vous n'avez pas
                 d'extensions de navigateurs qui bloquent l'acc\xE8s a Google Scripts (par exemple 'Privacy Badger').
                 Si en changeant vos param\xE8tres l'erreur persiste, merci de contacter la personne responsable.
@@ -6,17 +6,16 @@
             <table id="book-table" class="table table-striped table-sm table-hover">
               <thead>
                 <tr>
-                  <th scope="col">Cote</th>
+                  <th scope="col">Type de doc</th>
+                  <th scope="col">Cat\xE9gorie</th>
+                  <th scope="col">ISBN</th>
                   <th scope="col">Titre</th>
-                  <th scope="col">Auteur&middot;trice</th>
-                  <th scope="col">Date de publication</th>
-                  <th scope="col">Th\xE9matique</th>
-                  <th scope="col">Trigger</th>
-                  <th scope="col">Status</th>
-                  <th scope="col">Date d'emprunt</th>
+                  <th scope="col">Auteuricexs</th>
+                  <th scope="col">Ann\xE9es</th>
+                  <th scope="col">Maisons d'\xE9dition</th>
                 </tr>
               </thead>
               <tbody>
               </tbody>
             </table>
-        `}};async function l(){let e=await new r(n).getAndParseBooks();new s().fillElement(document.getElementById("books-div"),e)}l();})();
+        `}};async function i(){let e=await new r(n).getAndParseBooks();new o().fillElement(document.getElementById("books-div"),e)}i();})();
